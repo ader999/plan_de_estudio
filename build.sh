e79cd36841c319efa48da1acbe8e46e89566d1bf
overader <aderjasmirzeasrocha@gmail.com>
2023-10-03 23:16:37 -0600

    Actualizacion3 03/10/2023

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 #poetry install
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('overader', 'aderjasmirzeasrocha@gmail.com', '00040')" | python manage.py shell
+
 python manage.py migrate
+
+python manage.py collectstatic --no-input
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('overader', 'aderjasmirzeasrocha@gmail.com', '00040')" | python manage.py shell
