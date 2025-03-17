1d647529e6854798f874a6f706de11c80dd0ba4b
overaderLinux <aderjasmirzeasrocha@gmail.com>
2025-03-17 12:17:37 -0600

    Actualizado psycopg2-binary a 2.9.10 para compatibilidad con PostgreSQL 16.4 y añadido httpx como dependencia

--- plan_de_estudio/settings.py
+++ plan_de_estudio/settings.py
@@ -97,14 +97,7 @@ WSGI_APPLICATION = 'plan_de_estudio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-"""DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-}
-"""
+# Configuración para conectarse a PostgreSQL en Railway
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
